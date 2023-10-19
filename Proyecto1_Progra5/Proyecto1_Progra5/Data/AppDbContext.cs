48b73a4af2638635744631031288322c02453a3c
Pablo-S0 <113727347+Pablo-S0@users.noreply.github.com>
2023-10-18 18:21:14 -0600

    Add files via upload

---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto1_Progra5.Models;
+using System.Collections.Generic;
+
+namespace Proyecto1_Progra5.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
