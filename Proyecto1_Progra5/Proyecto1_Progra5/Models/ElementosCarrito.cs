05bf74bffab5c3ae6679bef66107f624ae40c9bb
Pablo-SO <pablosanchezocampo@gmail.com>
2023-10-20 16:11:37 -0600

    BD_L-R_Vistas

---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Models/ElementosCarrito.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Proyecto1_Progra5.Models
+{
+    public class ElementosCarrito
+    {
+
+        [Required] public int IDECarrito { get; set; }
+        [Required] public int IDCarrito { get; set; }
+        [Required] public int IDProducto { get; set; }
+        [Required] public int Cantidad { get; set; }
+        [Required] public int SubTotal { get; set; }
+
+    }
+}
---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Proyecto1_Progra5.Models
+{
+    public class Producto
+    {
+        [Key] public int IDProducto { get; set; }
+        [Required] public string Nombre { get; set; }
+        [Required] public string Foto { get; set; }
+        [Required] public float Precio { get; set; }
+        [Required] public string Descripcion { get; set; }
+    }
+}
