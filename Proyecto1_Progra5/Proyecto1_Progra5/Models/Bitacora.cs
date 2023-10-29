923103618b5fc9a211b547fa05e08292e03a158d
Pablo-SO <pablosanchezocampo@gmail.com>
2023-10-29 14:44:39 -0600

    CombiosDB

---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Models/Bitacora.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto1_Progra5.Models
+{
+    public class Bitacora
+    {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("IdBitacora")]
+        public int Id { get; set; }
+        [Required] public DateTime FechaHora { get; set; }
+        [Required] public String Descripcion { get; set; }
+
+
+    }
+}
---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Models/TipoProducto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Proyecto1_Progra5.Models
+{
+    public class TipoProducto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("IdTipoProducto")]
+        public int Id { get; set; }
+
+        [Required] public string Nombre { get; set; }
+    }
+}
