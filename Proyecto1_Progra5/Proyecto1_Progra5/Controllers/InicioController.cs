a20ae4a7ab5a1f38e55121582b95f7b399ac6f8d
Pablo-SO <pablosanchezocampo@gmail.com>
2023-10-31 16:22:37 -0600

    Login-Registrar-Vistas

---
+++ Proyecto1_Progra5/Proyecto1_Progra5/Controllers/InicioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto1_Progra5.Data;
+using Proyecto1_Progra5.Models;
+
+namespace Proyecto1_Progra5.Controllers
+{
+    public class InicioController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View("IniciarSesion"); // Muestra la vista de inicio de sesión 
+        }
+
+        public IActionResult Registrarse()
+        {
+            return View("Registrarse"); // Muestra la vista de registro
+        }
+    }
+}
